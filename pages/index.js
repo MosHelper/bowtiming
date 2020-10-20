38ca4cc6e95d8928236a0cba65a44fc450c5a5f9
moshelper <moshelper.com@gmail.com>
2020-10-20 21:33:01 +0700

    success

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,76 @@
+const moment = require('moment');
+import { Col, InputNumber, Row, TimePicker } from 'antd'
+import { List } from 'antd';
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react';
+import {
+  FieldTimeOutlined,
+  SwapRightOutlined,
+  UserSwitchOutlined
+} from '@ant-design/icons';
 
 export default function Home() {
+  const format = 'HH:mm';
+  const [startTime, setStartTime] = useState('');
+  const [employee, setEmployee] = useState('');
+
+  const handleEmp = (emp) => {
+    const arrEmp = [];
+    for (let i = 1; i <= emp; i++) {
+      arrEmp.push(i);
+    }
+    setEmployee(arrEmp);
+  }
+
+  const renderT = (m, i) => {
+    const sTime1 = moment(startTime.format(format), format);
+    const sTime2 = moment(startTime.format(format), format);
+    const mm1 = (i - 1) * m;
+    const mm2 = i * m;
+    return sTime1.add(mm1, 'minutes').format(format) + '-' + sTime2.add(mm2, 'minutes').format(format);
+  }
+
+  const renderTitle = (min, emp) => {
+    const sTime = moment(startTime.format(format), format);
+    const mm = emp.length * min;
+    return sTime.add(mm, 'minutes').format(format);
+  }
+
+  const renderList = (min) => {
+    return (startTime !== '' && employee !== '')
+      ? (
+        <Col key={min}>
+          <List
+            bordered
+            header={<h2><FieldTimeOutlined />{min}น.<SwapRightOutlined />{renderTitle(min, employee)}</h2>}
+            dataSource={employee}
+            renderItem={(i) => (
+              <List.Item key={i}>{renderT(min, i)}</List.Item>
+            )}
+          />
+        </Col>
+      )
+      : <div key={min}></div>
+  }
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main>
+        <div id="params">
+          <span className="brand">BowTiming</span>
+          <UserSwitchOutlined className="userIcon" /><InputNumber onChange={handleEmp} min="1" size="large" />
+          <TimePicker format={format} onChange={setStartTime} placeholder="Start At" size="large" />
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <Row>
+          {[10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60].map(min => renderList(min))}
+        </Row>
+      </main>
     </div>
   )
 }
